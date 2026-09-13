8f8c2571d93b1d46411b84ac6975ac1489899188
Maria Sobetska <muimop3sw@gmail.com>
2026-09-13 21:19:07 +0300

    Налаштовано Gulp для HTML, SCSS, JS та картинок

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const fileInclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+
+function html() {
+    return gulp.src('src/*.html')
+        .pipe(fileInclude())
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream())
+}
+function scss() {
+    return gulp.src('src/app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(browserSync.stream())
+}
+function js() {
+    return gulp.src('src/app/js/**/*.js')
+         .pipe(uglify())
+         .pipe(gulp.dest('dist/js/'))
+         .pipe(browserSync.stream())
+}
+function images() {
+    return gulp.src('src/app/imgs/**/*.*')
+         .pipe(imagemin())
+         .pipe(gulp.dest('dist/imgs/'))
+}
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
+}
+function watchFiles() {
+    gulp.watch('src/*.html', html)
+    gulp.watch('src/app/scss/**/*.scss', scss)
+    gulp.watch('src/app/js/**/*.js', js)
+    gulp.watch('src/app/imgs/**/*.*', images)
+}
+exports.default = gulp.series(html, scss, js, images,
+    gulp.parallel(serve, watchFiles))
